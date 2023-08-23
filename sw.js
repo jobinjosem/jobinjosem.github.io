f33f105b3d6152259c886961654214d829059b4d
jobinjosem <jobinjosem@hotmail.com>
2023-08-23 23:50:20 +0200

    wsl

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20230823.132541';
+const cacheName = 'chirpy-20230823.234741';
 
 function verifyDomain(url) {
     for (const domain of allowedDomains) {
